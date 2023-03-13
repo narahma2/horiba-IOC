63a4c35ed1811bc682bd67690842cd7654086a6a
Naveed Rahman <>
2023-03-13 15:19:11 -0500

    Added config, corrected turret+added tips on UI

--- scripts/start_ioc.py
+++ scripts/start_ioc.py
@@ -18,7 +18,7 @@ c = yaqc.Client(39876)
 init_position = c.get_position()
 init_slit = c.get_front_entrance_slit()
 init_mirror = c.get_exit_mirror()
-init_turret = 'low'
+init_turret = c.get_turret()
 
 # Set turret default to low
 c.set_turret(init_turret)
@@ -48,24 +48,34 @@ rbv_status_label = builder.stringIn(
                                     )
 
 # Wavelength position
-rbv_pos = builder.aIn('POSITION-RBV', initial_value=c.get_position())
+rbv_pos = builder.aIn(
+                      'POSITION-RBV',
+                      initial_value=c.get_position(),
+                      EGU='nm',
+                      PREC=2,
+                      )
 rec_pos = builder.aOut(
                        'POSITION',
                        initial_value=init_position,
                        on_update=lambda v: c.set_position(v),
                        DRVL=0,
-                       DRVH=1200,
+                       DRVH=3000,
+                       EGU='nm',
                        )
 
 # Entrance slit
 rbv_slit = builder.aIn(
                        'SLIT-WIDTH-RBV',
-                       initial_value=c.get_front_entrance_slit()
+                       initial_value=c.get_front_entrance_slit(),
+                       EGU='mm',
+                       PREC=2
                        )
 rec_slit = builder.aOut(
                         'SLIT-WIDTH',
                         initial_value=init_slit,
-                        on_update=lambda v: c.set_front_entrance_slit(v)
+                        on_update=lambda v: c.set_front_entrance_slit(v),
+                        EGU='mm',
+                        PREC=2
                         )
 
 # Exit mirror
@@ -107,7 +117,7 @@ async def update_status():
         rbv_slit.set(c.get_front_entrance_slit())
         rbv_mirror.set(c.get_exit_mirror())
         rbv_turret.set(c.get_turret())
-        await asyncio.sleep(0.5)
+        await asyncio.sleep(1)
 
 
 dispatcher(update_status)
